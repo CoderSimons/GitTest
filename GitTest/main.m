df351ba4d9cddca76aab9ef9663b073f4954b776
Simons <codersimons@gmail.com>
2016-06-07 23:08:02 +0800

    初始化项目

---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by 牛逸飞 on 16/6/7.
+//  Copyright © 2016年 MikeBoom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
