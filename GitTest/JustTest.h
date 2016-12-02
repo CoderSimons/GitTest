1d4babee8cdbd05f35a7c67bb741db4d2dd440df
Niu Yifei <yifei.niu@enai.im>
2016-12-02 14:12:06 +0800

    创建测试文件

---
+++ GitTest/JustTest.h
@@ -0,0 +1,13 @@
+//
+//  JustTest.h
+//  GitTest
+//
+//  Created by Simons on 2016/12/2.
+//  Copyright © 2016年 MikeBoom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JustTest : NSObject
+
+@end
---
+++ GitTest/JustTest.m
@@ -0,0 +1,13 @@
+//
+//  JustTest.m
+//  GitTest
+//
+//  Created by Simons on 2016/12/2.
+//  Copyright © 2016年 MikeBoom. All rights reserved.
+//
+
+#import "JustTest.h"
+
+@implementation JustTest
+
+@end
